9c2bfd12c7d2cb5a65e47c4b3d0db02980cab4b4
ashish <ashishkarhale@gmail.com>
2024-04-22 22:43:56 +0530

    Added first tasks

---
+++ dcelery/dcelery/celery.py
@@ -0,0 +1,14 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcelery.settings")
+
+app = Celery('dcelery')
+
+app.config_from_object('django.conf:settings',namespace = "CELERY")
+
+@app.task
+def add_numbers():
+    pass
+
+app.autodiscover_tasks()
